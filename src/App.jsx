68ee4b6dd5b4b8bea4666b513dcbad27669956a6
araujoleonardo310 <araujoleonardo310@gmail.com>
2022-10-27 14:26:19 -0300

    feat(components): criação de components

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/header";
+import Nav from "./components/nav";
+import About from "./components/about";
+import Experiences from "./components/experiences";
+import Portifolio from "./components/portifolio";
+import Testimonials from "./components/testimonials";
+import Contacts from "./components/experiences";
+import Footer from "./components/footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experiences />
+      <Portifolio />
+      <Testimonials />
+      <Contacts />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/experiences/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>experiences</div>;
+};
+
+export default index;
